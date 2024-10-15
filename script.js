3ca87466e9a99a4954419d87a28c587b7ff14c7d
Cesar <cesarnsingi@hotmail.com>
2024-10-15 05:48:47 +0400

    add other var when other option click

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let toastBox = document.getElementById('toastBox');
 
+let successMsg = 'Successfully submitted';
+let errorMsg = 'Please fix the error!'
+let invalidMsg = 'Invalid input, check again';
+
+
 function showToast(){
    let toast = document.createElement('div');
    toast.classList.add('toast');
